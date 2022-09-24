a16af3212cd9744d2738a2d22ae5023c9d5fbd0e
yourmindsetremodel <113265377+yourmindsetremodel@users.noreply.github.com>
2022-09-24 16:31:48 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,12 +23,33 @@ class App extends Component {
   }
 
   componentDidMount() {
-    const shuffledAnswerOptions = quizQuestions.map(question
+    const shuffledAnswerOptions = quizQuestions.map(question =>
+      this.shuffleArray(question.answers)
     );
     this.setState({
       question: quizQuestions[0].question,
       answerOptions: shuffledAnswerOptions[0]
     });
+  }
+
+  shuffleArray(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
   }
 
   handleAnswerSelected(event) {
